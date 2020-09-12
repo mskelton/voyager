42f9abca3c6b08a4a91ae4f378023e92e5e82bb2
Mark Skelton <mdskelton99@gmail.com>
2020-09-11 22:27:24 -0500

    Start working on log components

--- mobile/app/src/main/java/com/markskelton/voyager/ui/logs/LogListAdapter.kt
+++ mobile/app/src/main/java/com/markskelton/voyager/ui/logs/LogListAdapter.kt
@@ -1,4 +1,4 @@
-package com.markskelton.voyager.ui.loglist
+package com.markskelton.voyager.ui.logs
 
 import android.view.LayoutInflater
 import android.view.View
---
+++ mobile/app/src/main/java/com/markskelton/voyager/ui/logs/LogListFragment.kt
@@ -0,0 +1,57 @@
+package com.markskelton.voyager.ui.logs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.markskelton.voyager.databinding.FragmentLogListBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LogListFragment : Fragment() {
+
+    private lateinit var binding: FragmentLogListBinding
+    private lateinit var viewAdapter: LogListAdapter
+    private lateinit var viewManager: LinearLayoutManager
+    private val viewModel: LogListViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLogListBinding.inflate(layoutInflater)
+        initializeRecyclerView()
+        return binding.root
+    }
+
+    private fun initializeRecyclerView() {
+        viewManager = LinearLayoutManager(context)
+        viewAdapter = LogListAdapter()
+
+        binding.logListRecyclerView.apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            layoutManager = viewManager
+            adapter = viewAdapter
+
+            val itemDecoration = DividerItemDecoration(
+                binding.logListRecyclerView.context,
+                viewManager.orientation
+            )
+            addItemDecoration(itemDecoration)
+        }
+
+        viewModel.logs.observe(viewLifecycleOwner) { res ->
+            viewAdapter.entities = res
+            viewAdapter.notifyDataSetChanged()
+            binding.resource = res
+        }
+    }
+}
