bef4f3b3e9ef97d6353dbc05f24748e4c9f40f20
Mark Skelton <mdskelton99@gmail.com>
2020-09-11 22:39:50 -0500

    Add log list fragment to the home fragment

--- mobile/app/src/main/java/com/markskelton/voyager/ui/home/HomeFragment.kt
+++ mobile/app/src/main/java/com/markskelton/voyager/ui/home/HomeFragment.kt
@@ -4,28 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.markskelton.voyager.R
+import com.markskelton.voyager.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding;
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
     }
 }
