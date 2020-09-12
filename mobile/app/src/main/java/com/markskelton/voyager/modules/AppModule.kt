210fcbab9955df5fff95a2967bc91c2e05851d51
Mark Skelton <mdskelton99@gmail.com>
2020-09-11 22:33:07 -0500

    Turn off destructive migrations

--- mobile/app/src/main/java/com/markskelton/voyager/modules/AppModule.kt
+++ mobile/app/src/main/java/com/markskelton/voyager/modules/AppModule.kt
@@ -33,7 +33,6 @@ object AppModule {
     fun provideDb(app: Application): AppDatabase {
         return Room
             .databaseBuilder(app, AppDatabase::class.java, "app.db")
-            .fallbackToDestructiveMigration()
             .build()
     }
 
