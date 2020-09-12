5ff3a4eb5fc16a681b9dac53d3c73002b92b2970
Mark Skelton <mdskelton99@gmail.com>
2020-09-12 11:38:01 -0500

    Initialize dropdown and button

--- mobile/app/src/main/java/com/markskelton/voyager/ui/log/create/CreateLogFragment.kt
+++ mobile/app/src/main/java/com/markskelton/voyager/ui/log/create/CreateLogFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.markskelton.voyager.R
 import com.markskelton.voyager.databinding.FragmentCreateLogBinding
 
 class CreateLogFragment : Fragment() {
@@ -16,6 +20,23 @@ class CreateLogFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCreateLogBinding.inflate(layoutInflater)
+
+        initializeDropdown(binding.project)
+        initializeDropdown(binding.vehicle)
+        initializeCreateButton()
+
         return binding.root
+    }
+
+    private fun initializeDropdown(dropdown: AutoCompleteTextView) {
+        val items = listOf("Material", "Design", "Components", "Android")
+        val adapter = ArrayAdapter(requireContext(), R.layout.list_item, items)
+        dropdown.setAdapter(adapter)
+    }
+
+    private fun initializeCreateButton() {
+        binding.createButton.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
